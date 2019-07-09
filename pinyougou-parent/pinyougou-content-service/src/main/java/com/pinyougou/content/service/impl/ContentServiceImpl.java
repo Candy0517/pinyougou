ed0a50b0498581d983530bd35c64220432ae06de
duanxvsheng <duanxvsheng@126.com>
2019-07-09 20:11:24 +0800

    删除target

--- pinyougou-parent/pinyougou-content-service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
+++ pinyougou-parent/pinyougou-content-service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
@@ -1,5 +1,7 @@
 package com.pinyougou.content.service.impl;
+
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.github.pagehelper.Page;
@@ -11,113 +13,148 @@ import com.pinyougou.pojo.TbContentExample.Criteria;
 import com.pinyougou.content.service.ContentService;
 
 import entity.PageResult;
+import org.springframework.data.redis.core.RedisTemplate;
 
 /**
  * 服务实现层
- * @author Administrator
  *
+ * @author Administrator
  */
 @Service
 public class ContentServiceImpl implements ContentService {
 
-	@Autowired
-	private TbContentMapper contentMapper;
-	
-	/**
-	 * 查询全部
-	 */
-	@Override
-	public List<TbContent> findAll() {
-		return contentMapper.selectByExample(null);
-	}
+    @Autowired
+    private TbContentMapper contentMapper;
 
-	/**
-	 * 按分页查询
-	 */
-	@Override
-	public PageResult findPage(int pageNum, int pageSize) {
-		PageHelper.startPage(pageNum, pageSize);		
-		Page<TbContent> page=   (Page<TbContent>) contentMapper.selectByExample(null);
-		return new PageResult(page.getTotal(), page.getResult());
-	}
+    @Autowired
+    private RedisTemplate redisTemplate;
 
-	/**
-	 * 增加
-	 */
-	@Override
-	public void add(TbContent content) {
-		contentMapper.insert(content);		
-	}
+    /**
+     * 查询全部
+     */
+    @Override
+    public List<TbContent> findAll() {
+        return contentMapper.selectByExample(null);
+    }
 
-	
-	/**
-	 * 修改
-	 */
-	@Override
-	public void update(TbContent content){
-		contentMapper.updateByPrimaryKey(content);
-	}	
-	
-	/**
-	 * 根据ID获取实体
-	 * @param id
-	 * @return
-	 */
-	@Override
-	public TbContent findOne(Long id){
-		return contentMapper.selectByPrimaryKey(id);
-	}
+    /**
+     * 按分页查询
+     */
+    @Override
+    public PageResult findPage(int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        Page<TbContent> page = (Page<TbContent>) contentMapper.selectByExample(null);
+        return new PageResult(page.getTotal(), page.getResult());
+    }
 
-	/**
-	 * 批量删除
-	 */
-	@Override
-	public void delete(Long[] ids) {
-		for(Long id:ids){
-			contentMapper.deleteByPrimaryKey(id);
-		}		
-	}
-	
-	
-		@Override
-	public PageResult findPage(TbContent content, int pageNum, int pageSize) {
-		PageHelper.startPage(pageNum, pageSize);
-		
-		TbContentExample example=new TbContentExample();
-		Criteria criteria = example.createCriteria();
-		
-		if(content!=null){			
-						if(content.getTitle()!=null && content.getTitle().length()>0){
-				criteria.andTitleLike("%"+content.getTitle()+"%");
-			}
-			if(content.getUrl()!=null && content.getUrl().length()>0){
-				criteria.andUrlLike("%"+content.getUrl()+"%");
-			}
-			if(content.getPic()!=null && content.getPic().length()>0){
-				criteria.andPicLike("%"+content.getPic()+"%");
-			}
-			if(content.getStatus()!=null && content.getStatus().length()>0){
-				criteria.andStatusLike("%"+content.getStatus()+"%");
-			}
-	
-		}
-		
-		Page<TbContent> page= (Page<TbContent>)contentMapper.selectByExample(example);		
-		return new PageResult(page.getTotal(), page.getResult());
-	}
+    /**
+     * 增加
+     */
+    @Override
+    public void add(TbContent content) {
+        contentMapper.insert(content);
+        //清除缓存
+        redisTemplate.boundHashOps("content").delete(content.getCategoryId());
+    }
 
-	@Override
-	public List<TbContent> findByCategoryId(Long categoryId) {
 
-		TbContentExample example = new TbContentExample();
-		Criteria criteria = example.createCriteria();
-		criteria.andCategoryIdEqualTo(categoryId);//查询Id
-		criteria.andStatusEqualTo("1");//启用状态
-		example.setOrderByClause("sort_order");//排序
+    /**
+     * 修改
+     */
+    @Override
+    public void update(TbContent content) {
+        //获取修改前的对象ID
+        Long categoryId = contentMapper.selectByPrimaryKey(content.getId()).getCategoryId();
+        //删除缓存
+        redisTemplate.boundHashOps("content").delete(categoryId);
 
-		List<TbContent> tbContents = contentMapper.selectByExample(example);
+        contentMapper.updateByPrimaryKey(content);
 
-		return tbContents;
-	}
+
+        //因为Long是包装类，所以需要获得值，作比较
+        if (categoryId.longValue()!=content.getCategoryId().longValue()){
+            redisTemplate.boundHashOps("content").delete(content.getCategoryId());
+        }
+
+    }
+
+    /**
+     * 根据ID获取实体
+     *
+     * @param id
+     * @return
+     */
+    @Override
+    public TbContent findOne(Long id) {
+        return contentMapper.selectByPrimaryKey(id);
+    }
+
+    /**
+     * 批量删除
+     */
+    @Override
+    public void delete(Long[] ids) {
+        for (Long id : ids) {
+
+            Long categoryId = contentMapper.selectByPrimaryKey(id).getCategoryId();
+            redisTemplate.boundHashOps("content").delete(categoryId);
+
+            contentMapper.deleteByPrimaryKey(id);
+
+        }
+    }
+
+
+    @Override
+    public PageResult findPage(TbContent content, int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+
+        TbContentExample example = new TbContentExample();
+        Criteria criteria = example.createCriteria();
+
+        if (content != null) {
+            if (content.getTitle() != null && content.getTitle().length() > 0) {
+                criteria.andTitleLike("%" + content.getTitle() + "%");
+            }
+            if (content.getUrl() != null && content.getUrl().length() > 0) {
+                criteria.andUrlLike("%" + content.getUrl() + "%");
+            }
+            if (content.getPic() != null && content.getPic().length() > 0) {
+                criteria.andPicLike("%" + content.getPic() + "%");
+            }
+            if (content.getStatus() != null && content.getStatus().length() > 0) {
+                criteria.andStatusLike("%" + content.getStatus() + "%");
+            }
+
+        }
+
+        Page<TbContent> page = (Page<TbContent>) contentMapper.selectByExample(example);
+        return new PageResult(page.getTotal(), page.getResult());
+    }
+
+    @Override
+    public List<TbContent> findByCategoryId(Long categoryId) {
+
+        //先在缓存中查找，查找不到，再去数据库中查询，查找到后，再保存到缓存中
+        List<TbContent> contentList = (List<TbContent>) redisTemplate.boundHashOps("content").get(categoryId);
+
+        if (contentList == null) {
+
+            TbContentExample example = new TbContentExample();
+            Criteria criteria = example.createCriteria();
+            //查询Id
+            criteria.andCategoryIdEqualTo(categoryId);
+            //启用状态
+            criteria.andStatusEqualTo("1");
+            //排序
+            example.setOrderByClause("sort_order");
+
+            contentList = contentMapper.selectByExample(example);
+
+            redisTemplate.boundHashOps("content").put(categoryId, contentList);
+        }
+
+        return contentList;
+    }
 
 }
